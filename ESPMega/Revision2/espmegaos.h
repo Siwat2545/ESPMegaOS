926dfb896fee214208a788a5015e5afc8b5bbe1f
Siwat Sirichai <siwat@siwatinc.com>
2019-12-02 13:48:57 +0700

    Update espmegaos.h

--- ESPMega/Revision2/espmegaos.h
+++ ESPMega/Revision2/espmegaos.h
@@ -1,7 +1,17 @@
 #define VERSION 1.0.0
-#include emdio.h
-#include emadc.h
-#include emrom.h
-#include empowerpak.h
-#include emcomm.h
-#include emrtos
+
+//ESPMega Revision 2.0(a/b/c) Pin Map
+#define A0 0
+#define A1 1
+#define A2 2
+#define A3 3
+#define A4 4
+#define A5 5
+#define A6 6
+
+#include emdio.h //Digital IO Components - MCP23017
+#include emadc.h //Analog ADC Components - ADS1115
+#include emrom.h //On-Board EEPROM Components - HC16ES3F
+#include empowerpak.h //Siwat INC PowerPAK(tm) Components (PPVoltaic/PPThermoElectric/PPCCharge/PPLiPo)
+#include emcomm.h //NodeRED Communication Components (MQTT/Web Socket JS)
+#include emrtos //Real Time OS Components (Thread/FreeRTOS)
